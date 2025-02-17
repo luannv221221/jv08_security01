8eefeab7e33b5454ae8caa6e4a5c7bca35a3c84c
your new username <smallmonster900@gmail.com>
2025-02-17 20:25:12 +0700

    init

---
+++ src/main/java/com/ra/Sesssion01SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesssion01SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sesssion01SecurityApplication.class, args);
+    }
+
+}
